2f91890eca0d71f2d36b984f95305b7b6cbf16aa
BPitts8019 <bradleyj.pitts@gmail.com>
2020-01-09 17:55:13 -0700

    Created add, update, and remove db functions

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -15,10 +15,47 @@ const findById = (schemeId) => {
       .where("id", schemeId)
       .first();
 };
-const findSteps = (schemeId) => {};
-const add = (scheme) => {};
-const update = (changes, schemeId) => {};
-const remove = (schemeId) => {};
+const findSteps = (schemeId) => {
+   return db.select(`${STEPS}.id`, "scheme_name", "step_number", "instructions")
+      .from(STEPS)
+      .crossJoin(SCHEMES, "scheme_id", `${SCHEMES}.id`)
+      .where("scheme_id", schemeId)
+      .orderBy("step_number")
+};
+const add = async (scheme) => {
+   try {
+      const [id] = await db(SCHEMES).insert(scheme);
+      return findById(id);
+   } catch (error) {
+      return Promise.reject(error);
+   }
+};
+const update = async (changes, schemeId) => {
+   try {
+      await db(SCHEMES)
+         .where("id", schemeId)
+         .update(changes);
+      return findById(schemeId);
+   } catch (error) {
+      return Promise.reject(error);
+   }
+};
+const remove = async (schemeId) => {
+   try {
+      const scheme = await findById(schemeId);
+
+      if (scheme) {
+         await db(SCHEMES)
+            .where("id", schemeId)
+            .del();
+         return scheme;
+      } else {
+         throw new Error("Invalid ID");
+      }
+   } catch (error) {
+      return Promise.reject(error);
+   }
+};
 
 module.exports = {
    find,
